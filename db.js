0971c7f5cca6f7769dc5e710994580d58ed70a14
RyuuYou <67012095+RyuuYou0529@users.noreply.github.com>
2021-09-22 16:46:24 +0800

    docker

--- db.js
+++ db.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 
-const url = "mongodb://127.0.0.1/CPMS";
+//const url = "mongodb://127.0.0.1/CPMS";
+
+const url = "mongodb://mongo:27017/CPMS";
 
 mongoose.connect(
     url
 ).then(res=>{
-    console.log("connection success")
-}).catch(res=>{
-    console.log("connection error")
+    console.log("connection success");
+    console.log(res);
+}).catch(err=>{
+    console.log("connection error");
+    console.log(err);
 });
 
 module.exports = mongoose;
